e0d517265939adc4a0385f87c2d7dcc0ad35aad7
isabella hochschild <ihochschild@gmail.com>
2020-07-23 09:38:04 -0700

    Initial Commit

---
+++ MiniProject2Key/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiniProject2Key
+//
+//  Created by Isabella Hochschild on 7/23/20.
+//  Copyright © 2020 Isabella Hochschild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
